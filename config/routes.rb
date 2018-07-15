69b07510deb51cfd3ed6fc5ac3316bda61d3ce30
wananab1 <wtnbshut.mij@gmail.com>
2018-07-15 06:51:15 +0000

    test

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  namespace :admins do
-   resources :movies, except: [:show]
-  end
 
+  	namespace :admins do
+   		resources :movies, except: [:show]
+  	end
+  	resources :movies, only: [:show]
     devise_for :admins
     devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
     get '/search' => 'top#search'
    	root 'top#top'
